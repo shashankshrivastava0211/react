8022be9c8b907ed002c2ea82d7c98b2e0119bafa
shashankshrivastava0211 <160177951+shashankshrivastava0211@users.noreply.github.com>
2024-05-02 13:06:48 +0530

    accordian

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,11 +1,14 @@
 import AccordianList from "./AccordianList"
 
 const ResaurantCategory=({accordianData})=>{
-    
+    const handleClick =()=>{
+        
+
+    }
     return(
         <div>
         <div className="w-6/12  bg-gray-50  mx-auto my-4 shadow-lg p-4 ">
-            <div className="flex justify-between">
+            <div className="flex justify-between cursor-pointer" onClick={handleClick}>
             <span className="font-bold text-lg">{accordianData.title} ({accordianData.itemCards.length})</span>
                 <span className="text-center">⬇️</span>
             </div>
